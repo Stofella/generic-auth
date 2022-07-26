8e276d456229e0cc4513520de0bec68e603d189b
Lucas Stofella <lucas.stofella2@havan.com.br>
2022-07-26 20:34:42 -0300

    Contexto de autenticação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import type { NextPage } from 'next'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../../contexts/AuthContext';
+//import { FormStyles } from '../styles/global'
 
 const Home: NextPage = () => {
+  const [email, setEmail ] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email,
+      password,
+    }
+
+    await signIn(data)
+  }
+
   return (
-    <h1>Hello</h1>
+    <form onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+      <input type="setPassword" value={password} onChange={e => setPassword(e.target.value)}/>
+
+      <button type="submit">Entrar</button>
+    </form>
   )
 }
 
