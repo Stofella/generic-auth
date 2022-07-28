d63fe8dcf9bbe98c4ec9c1f995300f1fbd0d0b8e
Stofella <stofella@edu.univali.br>
2022-07-28 00:53:18 -0300

    Broadcast de logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,12 +5,15 @@ import { setupAPIClient } from "../../services/api"
 import { withSSRAuth } from "../../utils/withSSRAuth"
 
 export default function Dashboard () {
-    const { user} = useContext(AuthContext)
+    const { user , signOut } = useContext(AuthContext)
 
     return (
         <>
             <h1>Dashboard</h1>
             <h2>{user?.email}</h2>
+
+            <button onClick={signOut}>Sign out</button>
+
             <Can permissions={['metrics.list']}>
                 <div>Métricas</div>
             </Can>
