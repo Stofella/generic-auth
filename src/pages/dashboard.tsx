e1cd6e18abb37b82ed45371ef09f98eef5364150
Stofella <stofella@edu.univali.br>
2022-07-27 23:32:14 -0300

    Criando hook de permissão

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,15 +1,22 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/AuthContext"
+import { useCan } from "../../hooks/useCan"
 import { setupAPIClient } from "../../services/api"
 import { withSSRAuth } from "../../utils/withSSRAuth"
 
 export default function Dashboard () {
     const { user} = useContext(AuthContext)
 
+    const userCanSeeMetrics = useCan({
+        roles: ['administrator', 'editor']
+    });
+
     return (
         <>
             <h1>Dashboard</h1>
             <h2>{user?.email}</h2>
+
+            { userCanSeeMetrics && <div>Métricas</div>}
         </>
     )
 }
