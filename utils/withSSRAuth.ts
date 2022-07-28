4033a22be08a4cb6c45ada8b4cfef868e38dd2e6
Stofella <stofella@edu.univali.br>
2022-07-28 00:33:37 -0300

    Validando permissões (Server)

--- utils/withSSRAuth.ts
+++ utils/withSSRAuth.ts
@@ -1,12 +1,20 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import { destroyCookie, parseCookies } from "nookies";
+import decode from 'jwt-decode'
 import { AuthTokenError } from "../services/errors/AuthTokenError";
+import { validateUserPermissions } from "./validateUserPermissions";
 
-export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
+type withSSRAuthOptions = {
+    permissions: string[];
+    roles: string[];
+}
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>, options?: withSSRAuthOptions ) {
     return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> =>  {
         const cookies = parseCookies(ctx);
+        const token = cookies['genericAuth.token'];
 
-        if(!cookies['genericAuth.token']){
+        if(!token){
             return {
                 redirect: {
                     destination: '/',
@@ -14,6 +22,28 @@ export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
                 }
             }
         }
+
+        if(options){
+            const user = decode<{ permissions: string[], roles: string[]}>(token);
+            const { permissions, roles } = options
+            
+            const useHasValidPermissions = validateUserPermissions({
+                user,
+                permissions,
+                roles,
+            })
+
+            if(!useHasValidPermissions){
+                return {
+                    redirect: {
+                        destination: '/dashboard',
+                        permanent: false,
+                    }
+
+                }
+            }
+        }
+
         try{
             return await fn(ctx);
         }   catch(err){
