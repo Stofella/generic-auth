cac95505d4c3a9dc9e29e0c4878aa141d830e601
Stofella <stofella@edu.univali.br>
2022-07-27 21:16:55 -0300

    Realizando refresh do token

--- services/api.ts
+++ services/api.ts
@@ -1,11 +1,48 @@
-import axios from 'axios';
-import { parseCookies} from 'nookies'
+import axios, { AxiosError } from 'axios';
+import { parseCookies , setCookie} from 'nookies'
 
-const cookies = parseCookies();
+interface AxiosErrorResponse {
+  code?: string;
+}
+
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${cookies['genericAuth.token']}`
+  }
+})
+
+api.interceptors.response.use(response => {
+  return response;
+}, (error: AxiosError<AxiosErrorResponse>) => {
+  if(error.response?.status === 401) {
+    if(error.response.data?.code === 'token.expired'){ // renovar o token
+      cookies = parseCookies(); // atualizando valores do cookies
+
+      const { 'genericAuth.refreshToken': refreshToken } = cookies;
+
+      api.post('/refresh', {
+        refreshToken,
+      }).then(response => {
+        const { token } = response.data;
+
+        setCookie(undefined, 'genericAuth.token', token, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+  
+        setCookie(undefined, 'genericAuth.refreshToken', response.data.refreshToken, {
+          maxAge: 60 * 60 * 24 * 30, // 30 days
+          path: '/'
+        })
+        
+        api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+      })
+    }
+    else { //deslogar o usuario
+      
+    }
   }
 })
